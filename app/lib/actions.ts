96cffaa9f7998ffeacdea1ac4f4b61f32334767d
soma <swfh2367@gmail.com>
2024-04-03 22:41:15 +0900

    Revalidate and redirect

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,6 +2,8 @@
 
 import {z} from 'zod'
 import { sql } from '@vercel/postgres'
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
   id:z.string(),
@@ -27,4 +29,7 @@ export async function createInvoice(formData:FormData) {
     INSERT INTO invoices (cutomer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCente}, ${status}, ${date})
   `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
