08e2ffa03aa1e324a97cdeb63a14cae071ccb739
soma <swfh2367@gmail.com>
2024-04-03 22:20:18 +0900

    Extract the data from formData

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,12 @@
 'use server'
 
-export async function crateInvoice(formData:FormData) {
+export async function createInvoice(formData:FormData) {
+  const rawFormData = {
+    customerId:formData.get('cutomerId'),
+    amount:formData.get('amount'),
+    status:formData.get('status')
+  }
+
+  console.log(rawFormData)
   
 }
